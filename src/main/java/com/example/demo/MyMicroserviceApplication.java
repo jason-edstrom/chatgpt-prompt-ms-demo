a5a12d717be14f79f76dec18200ae9a9ffae746d
Jason Edstrom <jason@instacode.io>
2023-04-16 09:58:03 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/MyMicroserviceApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMicroserviceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MyMicroserviceApplication.class, args);
+  }
+
+
+}
